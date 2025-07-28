2b122b4eea591a2bd0ed5e52a6233b481909df21
nhatphanhk <114910072+nhatphanhk@users.noreply.github.com>
2025-07-28 11:09:21 +0700

    feat(language-switcher): file name error and add responsive mobile

--- src/components/layout/Language-switcher.tsx
+++ src/components/layout/Language-switcher.tsx
@@ -22,19 +22,23 @@ export function LanguageSwitcher() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="sm" className="hover:border-orange-600 gap-2">
+        <Button
+          variant="outline"
+          size="sm"
+          className="hover:border-orange-600 gap-2 px-2 sm:px-4 py-1 sm:py-2 min-w-0"
+        >
           <Globe className="h-4 w-4" />
-          <span className="hidden sm:inline-block">
+          <span className="hidden xs:inline-block sm:inline-block text-xs sm:text-base">
             {languages.find((lang) => lang.code === locale)?.name || 'Language'}{' '}
           </span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent align="end" className="min-w-[120px] w-32 sm:w-40" sideOffset={4}>
         {languages.map((language) => (
           <DropdownMenuItem
             key={language.code}
             onClick={() => setLocale(language.code as Locale)}
-            className={language.code === locale ? 'font-bold' : ''}
+            className={(language.code === locale ? 'font-bold ' : '') + 'text-xs sm:text-base'}
           >
             {language.name}
           </DropdownMenuItem>
